62d84e5ef28aced24f772c928173ef6b15b790bd
Rich'é <23082010042@student.upnjatim.ac.id>
2026-07-04 20:33:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll(
+".project-card,.education-card,.contact-card,.hero-text,.hero-image,.about-image,.about-content");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{
+    threshold:0.2
+});
+
+cards.forEach(card=>{
+    observer.observe(card);
+});
